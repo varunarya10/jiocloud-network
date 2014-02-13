29280e712109049cea04ea7a929aa0e306629092
hkumarmk <hkumarmk@gmail.com>
2014-02-13 14:45:14 +0000

    compute node issues fixed
    git-svn-id: https://10.135.121.138/svn/puppet_new_1/trunk/module_source/attachmentgenie-network-1.0.1@90 7e7508ed-7dfa-4282-9f22-b7e683a64a08

--- manifests/interfaces.pp
+++ manifests/interfaces.pp
@@ -7,7 +7,7 @@ class network::interfaces($bridged_if = eth0,$interfaces={}, $mappings={}, $auto
 ->
   file { '/etc/network/interfaces':
     content => template('network/interfaces.erb'),
-    notify  => Exec['network-restart'],
+#    notify  => Exec['network-restart'],
   }
 
   exec { 'network-restart':
